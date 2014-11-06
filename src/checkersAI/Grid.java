d53f62217b77f8744742c414197a7bb381b66f06
daajdk <mccarthyk1@students.rowan.edu>
2014-11-06 18:39:32 -0500

    test

---
+++ src/checkersAI/Grid.java
@@ -0,0 +1,30 @@
+
+public class Grid {
+	String str = "";
+	int location;
+	
+	// default constructor
+	public Grid(int i) {
+		this.location = i;
+		if (i < 10)
+			this.str = "" + i + "  ";
+		else
+			this.str = "" + i + " ";
+	}
+	
+	public void setStr(String piece) {
+		if (this.location < 10)
+			this.str = "" + piece + " ";
+		else
+			this.str = "" + piece + "";
+		//this.str = piece;
+	}
+	
+	public void resetStr() {
+		
+	}
+	
+	public String getGridString() {
+		return this.str;
+	}
+}
